4c3947a5b99f956af78d4b9333126d70263b7fc9
Evan Longwood <evanmlongwood@gmail.com>
2020-04-27 20:34:34 -0700

    made changes

--- nucampsite/src/components/CampsiteInfoComponent.js
+++ nucampsite/src/components/CampsiteInfoComponent.js
@@ -46,9 +46,11 @@ class CampsiteInfo extends Component {
   render() {
     if (this.props.campsite) {
       return (
+        <div className="container">
         <div className="row">
           {this.renderCampsite(this.props.campsite)}
           {this.renderComments(this.props.campsite.comments)}
+        </div>
         </div>
       );
     }
