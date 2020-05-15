27e1ae490bbba4645b0e30e9023687628dd39ca7
Evan Longwood <evanmlongwood@gmail.com>
2020-05-15 16:22:54 -0700

    Fetch Handling Errors

--- nucampsite/src/redux/ActionCreators.js
+++ nucampsite/src/redux/ActionCreators.js
@@ -14,9 +14,27 @@ export const addComment = (campsiteId, rating, author, text) => ({
 export const fetchCampsites = () => dispatch => {
   dispatch(campsitesLoading());
 
-  return fetch(baseUrl + 'campsites')
-  .then(response => response.json())
-  .then(campsites => dispatch(addCampsites(campsites)));
+  return fetch(baseUrl + "campsites")
+    .then(
+      response => {
+        if (response.ok) {
+          return response;
+        } else {
+          const error = new Error(
+            `Error ${response.status}: ${response.statusText}`
+          );
+          error.response = response;
+          throw error;
+        }
+      },
+      error => {
+        const errMess = new Error(error.message);
+        throw errMess;
+      }
+    )
+    .then(response => response.json())
+    .then(campsites => dispatch(addCampsites(campsites)))
+    .catch(error => dispatch(campsitesFailed(error.message)));
 };
 
 export const campsitesLoading = () => ({
@@ -33,10 +51,28 @@ export const addCampsites = campsites => ({
   payload: campsites
 });
 
-export const fetchComments = () => dispatch => {    
-  return fetch(baseUrl + 'comments')
-  .then(response => response.json())
-  .then(comments => dispatch(addComments(comments)));
+export const fetchComments = () => dispatch => {
+  return fetch(baseUrl + "comments")
+    .then(
+      response => {
+        if (response.ok) {
+          return response;
+        } else {
+          const error = new Error(
+            `Error ${response.status}: ${response.statusText}`
+          );
+          error.response = response;
+          throw error;
+        }
+      },
+      error => {
+        var errMess = new Error(error.message);
+        throw errMess;
+      }
+    )
+    .then(response => response.json())
+    .then(comments => dispatch(addComments(comments)))
+    .catch(error => dispatch(commentsFailed(error.message)));
 };
 
 export const commentsFailed = errMess => ({
@@ -52,10 +88,28 @@ export const addComments = comments => ({
 export const fetchPromotions = () => dispatch => {
   dispatch(promotionsLoading());
 
-  return fetch(baseUrl + 'promotions')
-  .then(response => response.json())
-  .then(promotions => dispatch(addPromotions(promotions)));
-}
+  return fetch(baseUrl + "promotions")
+    .then(
+      response => {
+        if (response.ok) {
+          return response;
+        } else {
+          const error = new Error(
+            `Error ${response.status}: ${response.statusText}`
+          );
+          error.response = response;
+          throw error;
+        }
+      },
+      error => {
+        const errMess = new Error(error.message);
+        throw errMess;
+      }
+    )
+    .then(response => response.json())
+    .then(promotions => dispatch(addPromotions(promotions)))
+    .catch(error => dispatch(promotionsFailed(error.message)));
+};
 
 export const promotionsLoading = () => ({
   type: ActionTypes.PROMOTIONS_LOADING
